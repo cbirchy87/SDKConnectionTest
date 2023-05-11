e7f89204be116f53bb8bf862757d500e5ab0ec35
Chris Birch <cbirchy87@gmail.com>
2023-05-11 09:51:39 +0100

    Add project files.

---
+++ SDKConnectionTest/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace SDKConnectionTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tbHost = new System.Windows.Forms.TextBox();
+            this.tbUsername = new System.Windows.Forms.TextBox();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tbHost
+            // 
+            this.tbHost.Location = new System.Drawing.Point(145, 6);
+            this.tbHost.Name = "tbHost";
+            this.tbHost.Size = new System.Drawing.Size(237, 22);
+            this.tbHost.TabIndex = 0;
+            this.tbHost.Text = "localhost";
+            // 
+            // tbUsername
+            // 
+            this.tbUsername.Location = new System.Drawing.Point(145, 34);
+            this.tbUsername.Name = "tbUsername";
+            this.tbUsername.Size = new System.Drawing.Size(237, 22);
+            this.tbUsername.TabIndex = 1;
+            this.tbUsername.Text = "System engineer";
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Location = new System.Drawing.Point(145, 62);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.PasswordChar = '*';
+            this.tbPassword.Size = new System.Drawing.Size(237, 22);
+            this.tbPassword.TabIndex = 2;
+            this.tbPassword.Text = "admin";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(145, 90);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(237, 23);
+            this.btnConnect.TabIndex = 3;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = ".....";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = ".....";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(4, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(22, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = ".....";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 123);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(503, 26);
+            this.statusStrip1.TabIndex = 7;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(151, 20);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(503, 149);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.tbUsername);
+            this.Controls.Add(this.tbHost);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbHost;
+        private System.Windows.Forms.TextBox tbUsername;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
---
+++ SDKConnectionTest/Form1.cs
@@ -0,0 +1,78 @@
+﻿using Paxton.Net2.OemClientLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Resources;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SDKConnectionTest
+{
+    public partial class Form1 : Form
+    {
+        public static ResourceManager rm;
+        public Form1()
+        {
+            InitializeComponent();
+
+            CultureInfo currentCulture = Thread.CurrentThread.CurrentCulture;
+            if (currentCulture.Name == "nl")
+            {
+                rm = new ResourceManager("SDKConnectionTest.nl_local", Assembly.GetExecutingAssembly());
+            }
+            else
+            {
+                rm = new ResourceManager("SDKConnectionTest.en_local", Assembly.GetExecutingAssembly());
+            }
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = rm.GetString("Connecting");
+            Task.Run(() =>
+            {
+                try
+                {
+                    OemClient c = new OemClient(tbHost.Text, 8025);
+                    var operatorList = c.GetListOfOperators();
+                    if (operatorList != null)
+                    {
+                        MessageBox.Show(rm.GetString("Connecting Sucessful"));
+                        toolStripStatusLabel1.Text = rm.GetString("Ready");
+
+                    }
+                    else
+                    {
+                        MessageBox.Show(rm.GetString("Failed to Connect"));
+                        toolStripStatusLabel1.Text = rm.GetString("Ready");
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    toolStripStatusLabel1.Text = rm.GetString("Ready");
+                }
+            });
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = rm.GetString("AppTitle");
+            btnConnect.Text = rm.GetString("Connect");
+            label1.Text = rm.GetString("Host");
+            label2.Text = rm.GetString("Username");
+            label3.Text = rm.GetString("Password");
+            toolStripStatusLabel1.Text = rm.GetString("Ready");
+            var x = rm.GetString("Ready");
+        }
+    }
+}
